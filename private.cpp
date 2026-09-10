418e03781b76e39a67bfd49eeda63892bf455240
Habel <habelk800@gmail.com>
2026-09-09 23:05:03 -0400

    Add private configuration variables

---
+++ private.cpp
@@ -0,0 +1,6 @@
+#include "private.h"
+
+const char* ssid = "Your SSID";
+const char* password = "Your SSID Password";
+const char* ntpServer = "pool.ntp.org";
+const char* apiURL = "Your OpenWeatherMap API Key";
